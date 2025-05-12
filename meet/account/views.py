7cd69f07c65eff2c2a5428038a65c69b401e6b02
ArsenAltunyan-alt <arsenaltunyan@knu.ua>
2025-05-12 17:56:23 +0300

    Refactor login view to handle user authentication and improve error messaging

--- meet/account/views.py
+++ meet/account/views.py
@@ -1,13 +1,30 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib import messages, auth
 
 
 # Create your views here.
 def main_page(request):
+    auth.logout(request)
+    messages.success(request, "Ви успішно вийшли з акаунту!")
     return render(request, 'account/main_page.html')
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, f"Вітаємо, {user.first_name}! Ви увійшли в систему.")
+            return redirect('/')
+        else:
+            messages.error(request, "Невірне ім’я користувача або пароль.")
+            return render(request, 'account/login.html')
+        
     return render(request, 'account/login.html')
 
 def register(request):
